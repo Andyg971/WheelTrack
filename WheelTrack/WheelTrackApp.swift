177750b03d6c87e4cd509d0775af8227474431aa
Andy <andy.grava@gmail.com>
2025-03-17 19:11:02 +0100

    Initial Commit

---
+++ WheelTrack/WheelTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheelTrackApp.swift
+//  WheelTrack
+//
+//  Created by Grava Andy on 17/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct WheelTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
