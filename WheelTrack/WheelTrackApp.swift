8ce9d9a2418663a6c6d25b69fdac302b9a062ad6
Andy <andy.grava@gmail.com>
2025-06-18 14:42:28 +0200

    🔧 Temporarily disable tutorial integration to fix compilation errors - Tutorial files need to be added to Xcode project manually

--- WheelTrack/WheelTrackApp.swift
+++ WheelTrack/WheelTrackApp.swift
@@ -10,19 +10,19 @@ import SwiftUI
 @main
 struct WheelTrackApp: App {
     @StateObject private var localizationService = LocalizationService.shared
-    @StateObject private var tutorialService = TutorialService.shared
+    // @StateObject private var tutorialService = TutorialService.shared // Temporairement commenté
     
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(localizationService)
-                .fullScreenCover(isPresented: $tutorialService.shouldShowTutorial) {
-                    TutorialView {
-                        // Callback quand le tutoriel se termine
-                        tutorialService.completeTutorial()
-                    }
-                    .environmentObject(localizationService)
-                }
+                // .fullScreenCover(isPresented: $tutorialService.shouldShowTutorial) {
+                //     TutorialView {
+                //         // Callback quand le tutoriel se termine
+                //         tutorialService.completeTutorial()
+                //     }
+                //     .environmentObject(localizationService)
+                // }
         }
     }
 }
